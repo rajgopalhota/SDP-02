24deb49acd3158bef8fe90bedd28045d1720d4f5
rajgopalhota <rajgopal@gmail.com>
2023-02-12 09:03:29 +0530

    Image slider added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Slider from '../ImageGallery/Slider'
 import './Styles/Homestyle.css'
 export default function Home() {
   return (
     <div>
-      <div className="home-container">
+      <Slider />
+      <div className="home-container-portfolio">
         <h1>Welocme to Autobots</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores autem voluptatibus saepe quod illo vitae repellendus ex at inventore deleniti, dolore ducimus, nam libero expedita eius vero sunt odio.
         Tenetur ducimus doloribus, totam deleniti temporibus voluptas, laudantium eos maiores dolore nesciunt, beatae impedit iure labore eligendi laborum consequuntur quaerat ullam veritatis. Error deserunt quas temporibus dolorum dolores incidunt in.
---
+++ client/src/components/ImageGallery/Slider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Gallery.css'
+import thar from './Gallery/thar.jpg'
+import mercedes from './Gallery/mercedes.jpg'
+import bugati from './Gallery/bugati.jpg'
+import suv from './Gallery/suv.jpg'
+export default function Slider() {
+  return (
+    <div className='image-slider-cont'>
+    <h1>Autobots Gallery</h1>
+      <div className="gallery-container">
+        <div className="gallery-wrapper">
+            <img src={thar} alt=''/>
+            <img src={mercedes} alt='' />
+            <img src={suv} alt='' />
+            <img src={bugati} alt='' />
+        </div>
+    </div>
+    </div>
+  )
+}
