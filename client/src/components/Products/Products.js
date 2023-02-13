aa8e8147bf32d58abb7c4948ca296bbcfeb6b160
rajgopalhota <rajgopal@gmail.com>
2023-02-13 16:46:34 +0530

    Product page

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,15 +1,140 @@
 import React from 'react'
-
+import './Styles/Style.css'
+import seat from './Assets/seat.png'
+import inmirror from './Assets/inmirror.png'
+import windmirror from './Assets/windmirror.jpg'
+import redhead from './Assets/redhead.jpg'
+import bluehead from './Assets/bluehead.jpg'
 export default function Products() {
     return (
-        <div>
-            <div className="home-container">
-                <h1>Welcome to our featured products</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem maiores autem voluptatibus saepe quod illo vitae repellendus ex at inventore deleniti, dolore ducimus, nam libero expedita eius vero sunt odio.
-                    Tenetur ducimus doloribus, totam deleniti temporibus voluptas, laudantium eos maiores dolore nesciunt, beatae impedit iure labore eligendi laborum consequuntur quaerat ullam veritatis. Error deserunt quas temporibus dolorum dolores incidunt in.
-                    Ipsa rem explicabo optio corporis incidunt voluptates neque animi, nam sint, rerum nulla minima distinctio pariatur voluptate minus quae commodi consequatur error quibusdam totam fugit. Blanditiis, doloribus. Ab, suscipit a.
-                    Enim velit soluta saepe cumque, provident illum ducimus tempore voluptatum, nisi molestias minima cupiditate omnis temporibus, dignissimos tenetur voluptates dolorum ab adipisci blanditiis nam ipsa possimus amet. Laudantium, eum dolorem.
-                </p>
+        <div className='ProductsPage'>
+            <div className="product-container">
+                {/* Card -1 */}
+                <div className="product-card">
+                    <div className="product-imgBx">
+                        <img src={seat} alt="Seat" />
+                    </div>
+                    <div className="product-contentBx">
+                        <h2>Nexa Seat</h2>
+                        <div className="product-size">
+                            <h3>Size :</h3>
+                            <span>7</span>
+                            <span>8</span>
+                            <span>9</span>
+                            <span>10</span>
+                        </div>
+                        <div className="product-color">
+                            <h3>Color :</h3>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <a href="/">Buy Now</a>
+                    </div>
+
+                </div>
+                {/* Card one ends */}
+                {/* Card -1 */}
+                <div className="product-card">
+                    <div className="product-imgBx">
+                        <img src={inmirror} alt="Seat" />
+                    </div>
+                    <div className="product-contentBx">
+                        <h2>Nexa Mirror</h2>
+                        <div className="product-size">
+                            <h3>Size :</h3>
+                            <span>7</span>
+                            <span>8</span>
+                            <span>9</span>
+                            <span>10</span>
+                        </div>
+                        <div className="product-color">
+                            <h3>Color :</h3>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <a href="/">Buy Now</a>
+                    </div>
+
+                </div>
+                {/* Card one ends */}
+                {/* Card -1 */}
+                <div className="product-card">
+                    <div className="product-imgBx">
+                        <img src={windmirror} alt="Seat" />
+                    </div>
+                    <div className="product-contentBx">
+                        <h2>Side Mirror</h2>
+                        <div className="product-size">
+                            <h3>Size :</h3>
+                            <span>7</span>
+                            <span>8</span>
+                            <span>9</span>
+                            <span>10</span>
+                        </div>
+                        <div className="product-color">
+                            <h3>Color :</h3>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <a href="/">Buy Now</a>
+                    </div>
+
+                </div>
+                {/* Card one ends */}
+                {/* Card -1 */}
+                <div className="product-card">
+                    <div className="product-imgBx">
+                        <img src={redhead} alt="Seat" />
+                    </div>
+                    <div className="product-contentBx">
+                        <h2>Red Headlight</h2>
+                        <div className="product-size">
+                            <h3>Size :</h3>
+                            <span>7</span>
+                            <span>8</span>
+                            <span>9</span>
+                            <span>10</span>
+                        </div>
+                        <div className="product-color">
+                            <h3>Color :</h3>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <a href="/">Buy Now</a>
+                    </div>
+
+                </div>
+                {/* Card one ends */}
+                {/* Card -1 */}
+                <div className="product-card">
+                    <div className="product-imgBx">
+                        <img src={bluehead} alt="Seat" />
+                    </div>
+                    <div className="product-contentBx">
+                        <h2>Blue Headlight</h2>
+                        <div className="product-size">
+                            <h3>Size :</h3>
+                            <span>7</span>
+                            <span>8</span>
+                            <span>9</span>
+                            <span>10</span>
+                        </div>
+                        <div className="product-color">
+                            <h3>Color :</h3>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <a href="/">Buy Now</a>
+                    </div>
+
+                </div>
+                {/* Card one ends */}
+                
             </div>
         </div>
     )
