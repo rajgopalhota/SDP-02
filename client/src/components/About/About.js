22b7411704f8d9dcc72ff67e02538a905cd1f322
rajgopalhota <rajgopal@gmail.com>
2023-02-13 16:48:02 +0530

    About page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,22 +1,30 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Style.css'
+import rajpic from './images/my pic.jpg'
+import gopalpic from "./images/gopalpic.jpg"
+import manojpic from "./images/manoj.jpg"
 export default function About() {
   return (
     <div className='Aboutani'>
       <div className="abt-container">
-        <h1>RAJGOPAL HOTA</h1>
+      <img src={gopalpic} alt=""></img>
+        <Link to="">RAJGOPAL HOTA</Link>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
         </p>
       </div>
       <div className="abt-container1">
-        <h1>RAJA RAJENDRA</h1>
+      <img src={rajpic} alt=""></img>
+        <Link to="">RAJA RAJENDRA</Link>
+        
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
         </p>
       </div>
       <div className="abt-container2">
-        <h1>Manoj</h1>
+      <img src={manojpic} alt=""></img>
+        <Link to="">NARAHARISETTI MANOJ</Link>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
         </p>
