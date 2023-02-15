5576def5dbcf5910253a99616fd01ec0bbe5f04f
rajgopalhota <rajgopal@gmail.com>
2023-02-15 14:14:16 +0530

    animations and abt page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -7,25 +7,31 @@ export default function About() {
   return (
     <div className='Aboutani'>
       <div className="abt-container">
-        <img src={gopalpic} alt=""></img>
+        <a href='https://www.instagram.com/raja.2k3/' target="_blank" rel="noreferrer"><img src={gopalpic} alt=""></img></a>
         <a href="https://www.linkedin.com/in/rajgopalhota/" target="_blank" rel="noreferrer">RAJGOPAL HOTA</a>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
+          <li>Animation effects</li>
+          <li>User Interface</li>
+          Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
         </p>
       </div>
       <div className="abt-container1">
-        <img src={rajpic} alt=""></img>
+        <a href='https://www.instagram.com/raja_.rajendra_/' target="_blank" rel="noreferrer"><img src={rajpic} alt=""></img></a>
         <a href="https://www.linkedin.com/in/raja-esha-hari-shanmuka-rajendra-772689248/" target="_blank" rel='noreferrer'>RAJA RAJENDRA</a>
 
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
+          <li>Animation effects</li>
+          <li>User Interface</li>
+          Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
         </p>
       </div>
       <div className="abt-container2">
-        <img src={manojpic} alt=""></img>
+        <a href='https://www.instagram.com/_mn.2k4_/' target="_blank" rel="noreferrer"><img src={manojpic} alt=""></img></a>
         <a href="https://www.linkedin.com/in/manojnaraharisetti/" target="_blank" rel="noreferrer">NARAHARISETTI MANOJ</a>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
+          <li>Animation effects</li>
+          <li>User Interface</li>
+          Dolore ullam provident iste quibusdam blanditiis explicabo, sed laborum molestiae debitis aliquam, accusamus necessitatibus, placeat in ex eligendi nemo soluta. Accusantium, illo.
         </p>
       </div>
     </div>
