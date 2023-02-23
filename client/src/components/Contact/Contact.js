ce7d2b4aab0f9e91ed5a838b832e03615c8f5a2a
Naraharisetti Manoj <110677973+manojnaraharisetti@users.noreply.github.com>
2023-02-23 19:21:55 +0530

    ntg

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -3,7 +3,22 @@ import './Styles/Contact.css'
 import location from './Assets/location.png'
 import email from './Assets/email.png'
 import phone from './Assets/phone.png'
+import { useForm } from "react-hook-form";
+
+
 export default function Contact() {
+
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        mode: "all",
+      });
+    
+      const onSubmit = (data) => console.log(data);
+
     const [sub, setSub] = useState("contactcomp-input-container")
     const handleonsignin = () => {
         setSub("contactcomp-input-container focus");
@@ -58,27 +73,79 @@ export default function Contact() {
                         <span className="circle one"></span>
                         <span className="circle two"></span>
 
-                        <form action="" autocomplete="off" className='contactcomp-formtag'>
+                        <form action="" autocomplete="off" className='contactcomp-formtag' onSubmit={handleSubmit(onSubmit)} >
                             <h3 className="contactcomp-title">Contact us</h3>
                             <div className={sub}>
-                                <input type="text" name="name" className="contactcomp-input" onClickCapture={handleonsignin} />
+                                <input type="text" name="name" className="contactcomp-input" onClickCapture={handleonsignin} 
+                                {...register("userName", {
+                                    required: "username is required",
+                                    minLength: {
+                                      value: 3,
+                                     message: "Username must be atleast 3 characters long...",
+                                    },
+                                    maxLength: {
+                                      value: 30,
+                                      message: "Username must be atmost 30 characters long...",
+                                    },
+                                  })}
+                                />
                                 <label for="">Username</label>
                                 <span>Username</span>
+                                <p id="kliu">{errors.userName?.message}</p>
+
                             </div>
                             <div className={sub}>
-                                <input type="email" name="email" className="contactcomp-input" onClickCapture={handleonsignin} />
+                                <input type="text" name="email" className="contactcomp-input" onClickCapture={handleonsignin} 
+                                {...register("email", {
+                                    required: "Email is Required...",
+                                    pattern: {
+                                      value:
+                                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                      message: "Email must be valid",
+                                    },
+                                  })}
+                                />
+                                <p id="kliu">{errors.email?.message}</p>
                                 <label for="">Email</label>
                                 <span>Email</span>
+
                             </div>
                             <div className={sub}>
-                                <input type="tel" name="phone" className="contactcomp-input" onClickCapture={handleonsignin} />
+                                <input type="tel" name="phone" className="contactcomp-input" onClickCapture={handleonsignin} 
+                                {...register("phone", {
+                                    required: "phone is Required...",
+                                    pattern: {
+                                        value: /^\d{10}$/,
+                                       message: "Must be valid",
+                                    },
+                                  })}
+                                />
                                 <label for="">Phone</label>
                                 <span>Phone</span>
+                                <p id="kliu">{errors.phone?.message}</p>
+
+                               
+
                             </div>
                             <div className={sub} textarea>
-                                <textarea name="message" className="contactcomp-input" onClickCapture={handleonsignin}></textarea>
+                                <textarea name="message" className="contactcomp-input" onClickCapture={handleonsignin}
+                                 {...register("text", {
+                                    required: "message is required",
+                                    minLength: {
+                                      value: 15,
+                                     message: "message must be atleast 15 characters long...",
+                                    },
+                                    maxLength: {
+                                      value: 40,
+                                      message: "message must be atmost 30 characters long...",
+                                    },
+                                  })}
+                                />
                                 <label for="">Message</label>
                                 <span>Message</span>
+                                <p id="kliu">{errors.text?.message}</p>
+
+
                             </div>
                             <input type="submit" value="Send" className="btn" />
                         </form>
