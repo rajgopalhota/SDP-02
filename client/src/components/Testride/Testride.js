f418e1582adfd5f8eaba5d794166c40d4e66467c
rajgopalhota <rajgopalhotanrg@gmail.com>
2023-03-07 13:51:22 +0530

    Test ride comp

---
+++ client/src/components/Testride/Testride.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Testride.css'
+
+export default function Testride() {
+    return (
+        <div>
+            <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <p>Try scrolling the rest of the page to see this option in action.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
