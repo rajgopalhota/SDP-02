44b838359a5c59fb74ce0d6a7eb0229cf569c656
rajgopalhota <rajgopalhotanrg@gmail.com>
2023-03-10 14:25:53 +0530

    background colors

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -58,7 +58,14 @@ export default function Cart(props) {
         <div className="modal-dialog">
           <div className="modal-content ">
             <div className="modal-header">
-              <h4 className="modal-title fs-5" id="exampleModalLabel">{auth.user}'s Cart</h4>
+              {
+                auth.user &&
+                <h4 className="modal-title fs-5" id="exampleModalLabel">{auth.user}'s Cart</h4>
+              }
+              {
+                !auth.user &&
+                <h4 className="modal-title fs-5" id="exampleModalLabel">Cart</h4>
+              }
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body row">
