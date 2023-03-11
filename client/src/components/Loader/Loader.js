f3faf3a8cdd831924e3a2dc909e9fe3cdb71e40e
Naraharisetti Manoj <110677973+manojnaraharisetti@users.noreply.github.com>
2023-03-11 16:58:23 +0530

    chg

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -3,23 +3,23 @@ import './Loader.css'
 export default function Loader() {
     return (
         <div className='LoaderBuffer'>
-            <div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
-                <div class="wheel"></div>
-                <div class="hamster">
-                    <div class="hamster__body">
-                        <div class="hamster__head">
-                            <div class="hamster__ear"></div>
-                            <div class="hamster__eye"></div>
-                            <div class="hamster__nose"></div>
+            <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+                <div className="wheel"></div>
+                <div className="hamster">
+                    <div className="hamster__body">
+                        <div className="hamster__head">
+                            <div className="hamster__ear"></div>
+                            <div className="hamster__eye"></div>
+                            <div className="hamster__nose"></div>
                         </div>
-                        <div class="hamster__limb hamster__limb--fr"></div>
-                        <div class="hamster__limb hamster__limb--fl"></div>
-                        <div class="hamster__limb hamster__limb--br"></div>
-                        <div class="hamster__limb hamster__limb--bl"></div>
-                        <div class="hamster__tail"></div>
+                        <div className="hamster__limb hamster__limb--fr"></div>
+                        <div className="hamster__limb hamster__limb--fl"></div>
+                        <div className="hamster__limb hamster__limb--br"></div>
+                        <div className="hamster__limb hamster__limb--bl"></div>
+                        <div className="hamster__tail"></div>
                     </div>
                 </div>
-                <div class="spoke"></div>
+                <div className="spoke"></div>
             </div>
         </div>
     )
