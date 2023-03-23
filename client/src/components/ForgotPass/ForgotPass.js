c62d87adbe6d1dda3cc9cc2bfcf60bf17a65a128
Esha Hari Shanmuka Rajendra Raja <rajarajendra925@gmail.com>
2023-03-23 18:55:53 +0530

    nt

--- client/src/components/ForgotPass/ForgotPass.js
+++ client/src/components/ForgotPass/ForgotPass.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import './Forgot.css'
 export default function ForgotPass() {
-    const [otp, setOtp] = useState(null);
-    const [pass, setPass] = useState(null);
+    const [otp, setOtp] = useState(true);
+    const [pass, setPass] = useState(true);
     return (
         <div>
             <div class="background">
@@ -12,27 +12,26 @@ export default function ForgotPass() {
             <form className='box'>
                 <h3>RESET PORTAL</h3>
                 <p>Enter your associated email.</p>
-
-                <input type="email" placeholder="Enter your Email" name='useremail' />
                 {
-                    otp &&
+                    !otp && !pass &&
+                    <>
+                        <input type="email" placeholder="Enter your Email" name='useremail' />
+                        <button type='submit'>Send OTP</button>
+                    </>
+                }
+                {
+                    otp && !pass &&
+                    <>
                     <input type="number" placeholder="Enter OTP" />
-                }
-                {
-                    pass && otp &&
-                    <input type="password" placeholder="Password" id="password" />
-                }
-                {
-                    !otp &&
-                    <button type='submit'>Send OTP</button>
-                }
-                {
-                    !pass && otp &&
                     <button type='submit'>Verify OTP</button>
+                    </>
                 }
                 {
                     pass && otp &&
+                    <>
+                    <input type="password" placeholder="Password" id="password" />
                     <button type='submit'>Change Password</button>
+                    </>
                 }
             </form>
         </div>
