e214b4ddc44d10a6c8fc609323366ef02a84cf13
rajgopalhota <2100032351@kluniversity.in>
2023-03-24 09:34:28 +0530

    Loader update

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -3,24 +3,13 @@ import './Loader.css'
 export default function Loader() {
     return (
         <div className='LoaderBuffer'>
-            <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-                <div className="wheel"></div>
-                <div className="hamster">
-                    <div className="hamster__body">
-                        <div className="hamster__head">
-                            <div className="hamster__ear"></div>
-                            <div className="hamster__eye"></div>
-                            <div className="hamster__nose"></div>
-                        </div>
-                        <div className="hamster__limb hamster__limb--fr"></div>
-                        <div className="hamster__limb hamster__limb--fl"></div>
-                        <div className="hamster__limb hamster__limb--br"></div>
-                        <div className="hamster__limb hamster__limb--bl"></div>
-                        <div className="hamster__tail"></div>
-                    </div>
-                </div>
-                <div className="spoke"></div>
-            </div>
+            <figure class="loading-loader">
+                <div class="loading-dot loading-white"></div>
+                <div class="loading-dot"></div>
+                <div class="loading-dot"></div>
+                <div class="loading-dot"></div>
+                <div class="loading-dot"></div>
+            </figure>
         </div>
     )
 }
